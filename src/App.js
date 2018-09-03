1820ece3f62bbeebee694eb4f8cd947c8f467c90
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-09-03 15:42:48 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import ToDo from './components/ToDo.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <ul>
+          <ToDo />
+          <Todo />
+        </ul>
       </div>
     );
   }
